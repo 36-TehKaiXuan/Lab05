8e7daa03f38d1a66c253d7b992de5d4064524d6a
36_TehKaiXuan <91871965+36-TehKaiXuan@users.noreply.github.com>
2021-10-12 16:44:04 +0800

    Lose Condition Added
    Player move to lose scene when they come in contact with the water. Cursor still locked after moving the lose scene. there is a script called mouse look controlling the cursor lock. need to find a way to set the lockcursor bool to false.

---
+++ Assets/Conditions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Conditions : MonoBehaviour
+{
+    private int Score;
+    public Text ScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text = "Score: " + Score;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Coin")
+        {
+            Score += 10;
+            Destroy(collider.gameObject);
+        }
+
+        //Lose Condition
+        if (collider.gameObject.tag == "Water")
+        {
+            //Currently lock not working
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+}
